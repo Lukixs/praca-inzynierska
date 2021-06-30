96ddb8e8f8a2a2b4e5b54007ee37fd0cf96cba0c
Lukixs <zeblukasz@gmail.com>
2021-06-30 17:07:57 +0200

    added highlighting of possible moves

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,8 +5,8 @@
       <div v-for="rowIndex in board.rowsNumber" :key="rowIndex">
         <div v-for="columnIndex in board.columnsNumber" :key="columnIndex" @click="cellOnClick(rowIndex, columnIndex)">
           <div :id="rowIndex*10+columnIndex" :class="[(rowIndex + columnIndex) % 2 === 0 ? 'white' : 'black']"> 
-            <div v-if="board.values[rowIndex-1][columnIndex-1] == 'biały'">&#9920;</div>
-            <div v-if="board.values[rowIndex-1][columnIndex-1] == 'czarny'">&#9922;</div>
+            <div v-if="board.values[rowIndex-1][columnIndex-1] == 'white'">&#9920;</div>
+            <div v-if="board.values[rowIndex-1][columnIndex-1] == 'black'">&#9922;</div>
           </div>
         </div>
       </div>
@@ -29,7 +29,7 @@ export default {
     return {
       tura: true,
       moveCounter: 1,
-      focused: false, //Mówi czy gracz wybrał piona którym chce ruszyć.
+      focused: null, //Mówi czy gracz wybrał piona którym chce ruszyć.
       board: {
         columnsNumber: 6,
         rowsNumber: 5,
@@ -40,11 +40,14 @@ export default {
   },
   methods: {
     cellOnClick(rowIndex, columnIndex){
-      if((this.board.values[rowIndex-1][columnIndex-1] == null) & (this.moveCounter<=24)){
+      if((this.board.values[rowIndex-1][columnIndex-1] == null) & (this.moveCounter<=4)){
         this.placePawn(rowIndex, columnIndex)
       }
-      else if ((this.board.values[rowIndex-1][columnIndex-1] != null) & (this.moveCounter>24)) {
+      else if ((this.board.values[rowIndex-1][columnIndex-1] != null) & (this.moveCounter>4) & (this.focused == null)) {
         this.selectPawn(rowIndex, columnIndex)
+      }
+      else if ((this.board.values[rowIndex-1][columnIndex-1] != null) & (this.moveCounter>4) & (this.focused != null)) {
+        this.reSelectPawn(rowIndex, columnIndex)
       }
     },
 
@@ -52,9 +55,9 @@ export default {
       if(this.board.values[rowIndex-1][columnIndex-1] == null){
         const newRow = this.board.values[rowIndex-1].slice(0)
         if(this.tura == true)
-          newRow[columnIndex-1] = "biały"
+          newRow[columnIndex-1] = "white"
         else
-        newRow[columnIndex-1] = "czarny"
+        newRow[columnIndex-1] = "black"
         this.tura = !this.tura
         this.$set(this.board.values, rowIndex-1, newRow)
 
@@ -67,9 +70,81 @@ export default {
     },
 
     selectPawn(rowIndex, columnIndex){
+      // const element = document.getElementById(rowIndex*10+columnIndex)
+      // element.classList.add("yellowgreen")
+      // console.log(rowIndex, columnIndex);
+      // console.log(element.children[0].textContent);
+      if(this.board.values[rowIndex-1][columnIndex-1] == "white" && this.tura % 2 == 1){
+        this.drawAvailableMoves(rowIndex,columnIndex)
+        // const element = document.getElementById(rowIndex*10+columnIndex)
+        // element.classList.add("yellowgreen")
+        this.focused = {rowIndex, columnIndex}
+        console.log(this.focused)
+      }
+      else if (this.board.values[rowIndex-1][columnIndex-1] == "black" && this.tura % 2 == 0){
+        this.drawAvailableMoves(rowIndex,columnIndex)
+        // const element = document.getElementById(rowIndex*10+columnIndex)
+        // element.classList.add("yellowgreen")
+        this.focused = {rowIndex, columnIndex}
+        console.log(this.focused)
+      }
+    },
+
+    reSelectPawn(rowIndex, columnIndex){
+      if(this.board.values[rowIndex-1][columnIndex-1] == "white" && this.tura % 2 == 1){
+        this.removeAvailableMoves(this.focused.rowIndex, this.focused.columnIndex)
+        this.drawAvailableMoves(rowIndex,columnIndex)
+        this.focused = {rowIndex, columnIndex}
+        console.log(this.focused)
+      }
+      else if (this.board.values[rowIndex-1][columnIndex-1] == "black" && this.tura % 2 == 0){
+        this.removeAvailableMoves(this.focused.rowIndex, this.focused.columnIndex)
+        this.drawAvailableMoves(rowIndex,columnIndex)
+        this.focused = {rowIndex, columnIndex}
+        console.log(this.focused)
+      }
+    },
+
+    drawAvailableMoves(rowIndex, columnIndex){
       const element = document.getElementById(rowIndex*10+columnIndex)
-      element.classList.add("yellowgreen")
-      console.log(rowIndex, columnIndex);
+      element.classList.add("darkgreen")
+      if(rowIndex-1 >= 1 & this.board.values[rowIndex-2][columnIndex-1] == null){
+        const element = document.getElementById((rowIndex-1)*10+columnIndex)
+        element.classList.add("yellowgreen")
+      }
+      if(rowIndex+1 <= this.board.rowsNumber & this.board.values[rowIndex][columnIndex-1] == null){
+        const element = document.getElementById((rowIndex+1)*10+columnIndex)
+        element.classList.add("yellowgreen")
+      }
+      if(columnIndex-1 >= 1 & this.board.values[rowIndex-1][columnIndex-2] == null){
+        const element = document.getElementById(rowIndex*10+(columnIndex-1))
+        element.classList.add("yellowgreen")
+      }
+      if(columnIndex+1 <= this.board.columnsNumber & this.board.values[rowIndex-1][columnIndex] == null){
+        const element = document.getElementById(rowIndex*10+(columnIndex+1))
+        element.classList.add("yellowgreen")
+      }
+    },
+
+    removeAvailableMoves(rowIndex, columnIndex){
+      const element = document.getElementById(rowIndex*10+columnIndex)
+      element.classList.remove("darkgreen")
+      if(rowIndex-1 >= 1 & this.board.values[rowIndex-2][columnIndex-1] == null){
+        const element = document.getElementById((rowIndex-1)*10+columnIndex)
+        element.classList.remove("yellowgreen")
+      }
+      if(rowIndex+1 <= this.board.rowsNumber & this.board.values[rowIndex][columnIndex-1] == null){
+        const element = document.getElementById((rowIndex+1)*10+columnIndex)
+        element.classList.remove("yellowgreen")
+      }
+      if(columnIndex-1 >= 1 & this.board.values[rowIndex-1][columnIndex-2] == null){
+        const element = document.getElementById(rowIndex*10+(columnIndex-1))
+        element.classList.remove("yellowgreen")
+      }
+      if(columnIndex+1 <= this.board.columnsNumber & this.board.values[rowIndex-1][columnIndex] == null){
+        const element = document.getElementById(rowIndex*10+(columnIndex+1))
+        element.classList.remove("yellowgreen")
+      }
     }
   },
   beforeMount() {
@@ -114,6 +189,10 @@ export default {
 
 .yellowgreen {
     background-color: yellowgreen;
+}
+
+.darkgreen {
+    background-color: darkgreen;
 }
 
 
