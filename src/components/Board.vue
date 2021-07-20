2d993000f396df3cac31975667b291be85007946
Lukixs <zeblukasz@gmail.com>
2021-07-20 19:17:37 +0200

    panws now can move,

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,8 +5,8 @@
       <div v-for="rowIndex in board.rowsNumber" :key="rowIndex">
         <div v-for="columnIndex in board.columnsNumber" :key="columnIndex" @click="cellOnClick(rowIndex, columnIndex)">
           <div :id="rowIndex*10+columnIndex" :class="[(rowIndex + columnIndex) % 2 === 0 ? 'white' : 'black']"> 
-            <div v-if="board.values[rowIndex-1][columnIndex-1] == 'white'">&#9920;</div>
-            <div v-if="board.values[rowIndex-1][columnIndex-1] == 'black'">&#9922;</div>
+            <div v-if="board.values[rowIndex-1][columnIndex-1].player == 'white'">&#9920;</div>
+            <div v-if="board.values[rowIndex-1][columnIndex-1].player == 'black'">&#9922;</div>
           </div>
         </div>
       </div>
@@ -29,7 +29,8 @@ export default {
     return {
       tura: true,
       moveCounter: 1,
-      focused: null, //Mówi czy gracz wybrał piona którym chce ruszyć.
+      firstStageMovesLimit: 4, 
+      focused: null, // {rowIndex, columnIndex}   Aktualnie wybrany pionek
       board: {
         columnsNumber: 6,
         rowsNumber: 5,
@@ -41,32 +42,32 @@ export default {
   },
   methods: {
     cellOnClick(rowIndex, columnIndex){
-      if((this.board.values[rowIndex-1][columnIndex-1] == null) & (this.moveCounter<=4)){
+      if((this.moveCounter <= this.firstStageMovesLimit) & (this.board.values[rowIndex-1][columnIndex-1].player == null)){
         this.placePawn(rowIndex, columnIndex)
       }
-      else if ((this.board.values[rowIndex-1][columnIndex-1] != null) & (this.moveCounter>4) & (this.focused == null)) {
+      else if ((this.moveCounter > this.firstStageMovesLimit) & (this.focused == null) & (this.board.values[rowIndex-1][columnIndex-1].player != null)) {
         this.selectPawn(rowIndex, columnIndex)
       }
-      else if ((this.board.values[rowIndex-1][columnIndex-1] != null) & (this.moveCounter>4) & (this.focused != null)) {
+      else if ((this.moveCounter > this.firstStageMovesLimit) & (this.focused != null) & (this.board.values[rowIndex-1][columnIndex-1].player != null)) {
         this.reSelectPawn(rowIndex, columnIndex)
+      }
+      else if ((this.moveCounter > this.firstStageMovesLimit) & (this.focused != null) & (this.board.values[rowIndex-1][columnIndex-1].player == null)){
+        this.tryToMovePawnTo(rowIndex, columnIndex);
       }
     },
 
     placePawn(rowIndex, columnIndex){
-      if(this.board.values[rowIndex-1][columnIndex-1] == null){
+      if(this.board.values[rowIndex-1][columnIndex-1].player == null){
         const newRow = this.board.values[rowIndex-1].slice(0)
         if(this.tura == true)
-          newRow[columnIndex-1] = "white"
+          newRow[columnIndex-1] = { player: 'white', pawnIndex: ((this.moveCounter + 1) / 2) }
         else
-        newRow[columnIndex-1] = "black"
+        newRow[columnIndex-1] = { player: 'black', pawnIndex: (this.moveCounter / 2) }
         this.tura = !this.tura
         this.$set(this.board.values, rowIndex-1, newRow)
 
 
         this.moveCounter++
-        // this.board.values[rowIndex-1][columnIndex-1] = "XD"
-        // console.log(this.board.values)
-        // alert("Kliknięto wiersz: " + rowIndex + " kolumny: " + columnIndex)
       }
     },
 
@@ -75,53 +76,55 @@ export default {
       // element.classList.add("yellowgreen")
       // console.log(rowIndex, columnIndex);
       // console.log(element.children[0].textContent);
-      if(this.board.values[rowIndex-1][columnIndex-1] == "white" && this.tura % 2 == 1){
+      if(this.board.values[rowIndex-1][columnIndex-1].player == 'white' && this.tura % 2 == 1){
         this.drawAvailableMoves(rowIndex,columnIndex)
         // const element = document.getElementById(rowIndex*10+columnIndex)
         // element.classList.add("yellowgreen")
-        this.focused = {rowIndex, columnIndex}
+        this.focused = {rowIndex: rowIndex, columnIndex: columnIndex}
         console.log(this.focused)
       }
-      else if (this.board.values[rowIndex-1][columnIndex-1] == "black" && this.tura % 2 == 0){
+      else if (this.board.values[rowIndex-1][columnIndex-1].player == 'black' && this.tura % 2 == 0){
         this.drawAvailableMoves(rowIndex,columnIndex)
         // const element = document.getElementById(rowIndex*10+columnIndex)
         // element.classList.add("yellowgreen")
-        this.focused = {rowIndex, columnIndex}
+        this.focused = {rowIndex: rowIndex, columnIndex: columnIndex}
         console.log(this.focused)
       }
     },
 
     reSelectPawn(rowIndex, columnIndex){
-      if(this.board.values[rowIndex-1][columnIndex-1] == "white" && this.tura % 2 == 1){
-        this.removeAvailableMoves(this.focused.rowIndex, this.focused.columnIndex)
-        this.drawAvailableMoves(rowIndex,columnIndex)
-        this.focused = {rowIndex, columnIndex}
-        console.log(this.focused)
-      }
-      else if (this.board.values[rowIndex-1][columnIndex-1] == "black" && this.tura % 2 == 0){
-        this.removeAvailableMoves(this.focused.rowIndex, this.focused.columnIndex)
-        this.drawAvailableMoves(rowIndex,columnIndex)
-        this.focused = {rowIndex, columnIndex}
-        console.log(this.focused)
+      if(rowIndex != this.focused.rowIndex || columnIndex != this.focused.columnIndex){
+        if(this.board.values[rowIndex-1][columnIndex-1].player == 'white' && this.tura % 2 == 1){
+          this.removeAvailableMoves(this.focused.rowIndex, this.focused.columnIndex)
+          this.drawAvailableMoves(rowIndex,columnIndex)
+          this.focused = {rowIndex, columnIndex}
+          console.log(this.focused)
+        }
+        else if (this.board.values[rowIndex-1][columnIndex-1].player == 'black' && this.tura % 2 == 0){
+          this.removeAvailableMoves(this.focused.rowIndex, this.focused.columnIndex)
+          this.drawAvailableMoves(rowIndex,columnIndex)
+          this.focused = {rowIndex, columnIndex}
+          console.log(this.focused)
+        }
       }
     },
 
     drawAvailableMoves(rowIndex, columnIndex){
       const element = document.getElementById(rowIndex*10+columnIndex)
       element.classList.add("darkgreen")
-      if(rowIndex-1 >= 1 & this.board.values[rowIndex-2][columnIndex-1] == null){
+      if(rowIndex-1 >= 1 & this.board.values[rowIndex-2][columnIndex-1].player == null){
         const element = document.getElementById((rowIndex-1)*10+columnIndex)
         element.classList.add("yellowgreen")
       }
-      if(rowIndex+1 <= this.board.rowsNumber & this.board.values[rowIndex][columnIndex-1] == null){
+      if(rowIndex+1 <= this.board.rowsNumber & this.board.values[rowIndex][columnIndex-1].player == null){
         const element = document.getElementById((rowIndex+1)*10+columnIndex)
         element.classList.add("yellowgreen")
       }
-      if(columnIndex-1 >= 1 & this.board.values[rowIndex-1][columnIndex-2] == null){
+      if(columnIndex-1 >= 1 & this.board.values[rowIndex-1][columnIndex-2].player == null){
         const element = document.getElementById(rowIndex*10+(columnIndex-1))
         element.classList.add("yellowgreen")
       }
-      if(columnIndex+1 <= this.board.columnsNumber & this.board.values[rowIndex-1][columnIndex] == null){
+      if(columnIndex+1 <= this.board.columnsNumber & this.board.values[rowIndex-1][columnIndex].player == null){
         const element = document.getElementById(rowIndex*10+(columnIndex+1))
         element.classList.add("yellowgreen")
       }
@@ -130,26 +133,51 @@ export default {
     removeAvailableMoves(rowIndex, columnIndex){
       const element = document.getElementById(rowIndex*10+columnIndex)
       element.classList.remove("darkgreen")
-      if(rowIndex-1 >= 1 & this.board.values[rowIndex-2][columnIndex-1] == null){
+      if(rowIndex-1 >= 1 & this.board.values[rowIndex-2][columnIndex-1].player == null){
         const element = document.getElementById((rowIndex-1)*10+columnIndex)
         element.classList.remove("yellowgreen")
       }
-      if(rowIndex+1 <= this.board.rowsNumber & this.board.values[rowIndex][columnIndex-1] == null){
+      if(rowIndex+1 <= this.board.rowsNumber & this.board.values[rowIndex][columnIndex-1].player == null){
         const element = document.getElementById((rowIndex+1)*10+columnIndex)
         element.classList.remove("yellowgreen")
       }
-      if(columnIndex-1 >= 1 & this.board.values[rowIndex-1][columnIndex-2] == null){
+      if(columnIndex-1 >= 1 & this.board.values[rowIndex-1][columnIndex-2].player == null){
         const element = document.getElementById(rowIndex*10+(columnIndex-1))
         element.classList.remove("yellowgreen")
       }
-      if(columnIndex+1 <= this.board.columnsNumber & this.board.values[rowIndex-1][columnIndex] == null){
+      if(columnIndex+1 <= this.board.columnsNumber & this.board.values[rowIndex-1][columnIndex].player == null){
         const element = document.getElementById(rowIndex*10+(columnIndex+1))
         element.classList.remove("yellowgreen")
       }
+    },
+
+    tryToMovePawnTo(rowIndex, columnIndex){
+      console.log('Nowy ruch na:', rowIndex, columnIndex, this.focused);
+      if(((rowIndex == this.focused.rowIndex)  && (columnIndex == this.focused.columnIndex-1 || columnIndex == this.focused.columnIndex+1)) 
+      || (columnIndex == this.focused.columnIndex)  && (rowIndex == this.focused.rowIndex-1 || rowIndex == this.focused.rowIndex+1)){
+        //if(true){//Tutaj czy spalone sprawdzić historie
+          let newRow = this.board.values[rowIndex-1].slice(0);
+          newRow[columnIndex-1] = {player: this.board.values[this.focused.rowIndex-1][this.focused.columnIndex-1].player, pawnIndex: this.board.values[this.focused.rowIndex-1][this.focused.columnIndex-1].pawnIndex};
+          this.$set(this.board.values, rowIndex-1, newRow);
+          console.log('player', this.board.values[this.focused.rowIndex-1][this.focused.columnIndex-1].player, this.focused);
+          
+          
+          
+          let oldRow = this.board.values[this.focused.rowIndex-1].slice(0)
+          oldRow[this.focused.columnIndex-1] = { player: null, pawnIndex: null };
+          this.$set(this.board.values, this.focused.rowIndex-1, oldRow)
+
+          this.tura = !this.tura;
+          this.moveCounter++;
+
+          console.log('Powodzenie')
+        //}
+      }
+      //
     }
   },
   beforeMount() {
-    this.board.values = Array(8).fill(null).map(() => Array(8).fill(null))
+    this.board.values = Array(8).fill(null).map(() => Array(8).fill({ player: null, pawnIndex: null }));
   }
 }
 </script>
