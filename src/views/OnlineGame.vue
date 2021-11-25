242eccdfad0d352a0e693a53fdf4841bf1355575
Lukixs <zeblukasz@gmail.com>
2021-11-25 13:31:22 +0100

    [Fetature] Implemented Online Game vie Websocket (#8)
    * Created new component
    
    * Implemented working socket io connection
    
    * Moved connection procedure to component
    
    * Addde pawn moving listening, and pawn placing, created early lobby mechanic, pawn removing and emiting pawn move yet need to be added
    
    * Finnished basic game operations
    
    * Added name, input not implemented yet

---
+++ src/views/OnlineGame.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <OnlineGame />
+  </div>
+</template>
+<script>
+import OnlineGame from "@/components/OnlineGame.vue";
+export default {
+  name: "Online",
+  components: {
+    OnlineGame,
+  },
+};
+</script>
