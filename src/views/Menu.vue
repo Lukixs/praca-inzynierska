5ae3608539bcfa5cba07a27a57b8f1030f34025d
Łukasz Zebzda <zeblukasz@gmail.com>
2021-12-30 16:34:43 +0100

    Fixed sass, half-made menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="menu">
-    <h1 class="h1 title ">DARA</h1>
-    <div class="menu--options">
-      <v-btn
-        v-for="(item, index) in items"
-        :key="index"
-        x-large
-        dark
-        text
-        :to="item.link"
-        >{{ item.title }}</v-btn
-      >
+    <div class="header">
+      <h1 class="h1 title ">DARA</h1>
+    </div>
+
+    <div class="options">
+      <div class="options--list">
+        <!-- <div class="test1"> -->
+        <v-btn
+          dark
+          text
+          v-for="(item, index) in items"
+          :key="index"
+          :to="item.link"
+        >
+          {{ item.title }}</v-btn
+        >
+        <!-- </div> -->
+      </div>
     </div>
     <!-- <router-link to="/local">Local Game</router-link> |
     <router-link to="/about">AI Game - Easy</router-link> |
@@ -30,7 +37,7 @@ export default Vue.extend({
     return {
       items: [
         { title: "GRAJ LOKALNIE", link: "/local" },
-        { title: "GRAJ ONLINE", ilink: "/about" },
+        { title: "GRAJ ONLINE", link: "/about" },
         { title: "OPCJE", link: "/minimax" },
         { title: "INSTRUKCJA", icon: "mdi-forum", link: "/online" },
       ],
@@ -41,6 +48,63 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .menu {
   color: white;
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
+}
+.header {
+  font-size: 130px;
+  letter-spacing: -41px;
+}
+
+.options {
+  margin-top: 5vh;
+  font-size: 50px;
+  display: flex;
+
+  justify-content: center;
+  &--list {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  // .test1 {
+  //   position: absolute;
+  // }
+  // .test1:hover::before,
+  // .test1:hover::after {
+  //   content: "";
+  //   position: absolute;
+  //   height: 100%;
+  //   width: 20px;
+  //   top: 0px;
+  //   background-image: radial-gradient(
+  //       circle at center,
+  //       red 5px,
+  //       transparent 5px
+  //     ),
+  //     radial-gradient(circle at center, red 5px, transparent 5px);
+  //   background-size: 20px 20px;
+  //   background-position: top center, bottom center;
+  //   background-repeat: no-repeat;
+  // }
+
+  // .test1:hover::before {
+  //   left: 0px;
+  // }
+  // .test1:hover::after {
+  //   right: 0px;
+  // }
+
+  .v-btn {
+    // margin: 5px 0;
+    // width: 36px;
+    // min-width: 36px;
+    font-size: 50px;
+    height: 110px !important;
+  }
 }
 /* @import "../styles/styles.scss"; */
 </style>
